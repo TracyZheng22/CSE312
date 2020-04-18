019b2e8ee7de24e5ea5f5f187c82b7427df9b3df
Xurya <xur1@bxscience.edu>
2020-04-18 13:37:12 -0400

    #11 websocket message only
    Multimedia is not implemented yet.

--- UserPage.js
+++ UserPage.js
@@ -1,3 +1,30 @@
+let socket = new WebSocket('ws://' + window.location.host + '/websocket');
+if(socket.readyState != WebSocket.CLOSED){
+    console.log("Connected!");
+}else{
+    console.log("Closed!");
+}
+//Ryan note: From homework 7 for websocket reference. Please remember to correctly format input in the server 
+//For security (change tags to html versions, etc.)
+//
+//socket.onmessage = renderMessages;
+//function renderMessages(message) {
+//    let text = message.data;
+//    console.log("recieved: " + text);
+//    let tag = document.createElement("p");
+//    tag.innerHTML = text;
+//    let div = document.getElementById("chatHistory");
+//    div.appendChild(tag);
+//} 
+
+function sendMessage() {    
+    message = document.getElementById("formmsg").value;
+    console.log("send: " + message);
+    socket.send(message);
+    var reset = document.getElementById("formmsg");
+    reset.value = reset.defaultValue;
+}
+
 var coll = document.getElementsByClassName("collapse");
 for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
