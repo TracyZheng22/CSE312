85673340825b6d1747742b6236ce6389ae74812d
Xurya <xur1@bxscience.edu>
2020-04-20 19:27:38 -0400

    Maven eclipse changes
    This was automatically generated, commited just in case this messes up anyone else's configuration. Please let me know if that is the case!

--- target/classes/target/classes/landing.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("login").onclick = function () {
-       window.location.replace("Sign-in.html");
-};
-
-document.getElementById("regist").onclick = function () {
-        location.href = "Register.html";
-};
--- target/classes/target/classes/target/classes/sign.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("phase2").onclick = function () {
-        location.href = "UserPage.html";
-};
